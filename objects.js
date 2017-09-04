baf4eaf33a6f5d25f00a06c05b0ed211c825e64c
Robert W <robwash@gmail.com>
2017-09-04 20:01:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ playlist = { artistNames: "song titles"}
 
 function updatePlaylist(playlist, "artistName", songTitle){
 
-  playlist = { artistName: songTitle}
+  playlist = { "artistName": songTitle}
     return playlist
 
 }
