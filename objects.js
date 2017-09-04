dfe633c1cf45bfb094853b619f14bfbd755c8743
Robert W <robwash@gmail.com>
2017-09-04 20:52:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -13,7 +13,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 }
 function removeFromPlaylist(playlist, artistName){
-
+  playlist[artistName] = songTitle
   playlist = delete playlist.artistName
       return playlist[artistName]
 
